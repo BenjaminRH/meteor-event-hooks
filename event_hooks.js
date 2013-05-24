3ade3b76466e7d53739352ad394c79b642c3370d
Benjamin Harris <benjamin3harris@gmail.com>
2013-05-24 04:17:08 +0300

    Fix 'this' bug

--- event_hooks.js
+++ event_hooks.js
@@ -4,19 +4,19 @@ Hooks = {
 
 		// Lose focus
 		window.onblur = function () {
-			if (this.onLoseFocus !== undefined) this.onLoseFocus(); // Fire the event on the client
+			if (Hooks.onLoseFocus !== undefined) Hooks.onLoseFocus(); // Fire the event on the client
 			Meteor.call('eventsOnLoseFocus'); // Fire the event on the server
 		}
 
 		// Gain focus
 		window.onfocus = function () {
-			if (this.onGainFocus !== undefined) this.onGainFocus(); // Fire the event on the client
+			if (Hooks.onGainFocus !== undefined) Hooks.onGainFocus(); // Fire the event on the client
 			Meteor.call('eventsOnGainFocus'); // Fire the event on the server
 		}
 
 		// Close window/tab
 		var handleClose = function() {
-			if (this.onCloseSession !== undefined) this.onCloseSession(); // Fire the event on the client
+			if (Hooks.onCloseSession !== undefined) Hooks.onCloseSession(); // Fire the event on the client
 			Meteor.call('eventsOnCloseSession'); // Fire the event on the server
 		}
 		if (window.onunload !== undefined) window.onunload = function() { handleClose(); }
